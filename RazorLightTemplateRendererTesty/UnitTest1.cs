748aac34ad4cf57774cba2fbebda4896e2d279ba
Gregoire Poncelet <gregoire.poncelet@neuroglia.io>
2022-11-17 15:37:23 +0100

    test(test): all in one file

--- RazorLightTemplateRendererTesty/UnitTest1.cs
+++ RazorLightTemplateRendererTesty/UnitTest1.cs
@@ -1,29 +1,41 @@
+using FluentAssertions;
 using RazorLight;
-using System.Reflection;
+using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace RazorLightTemplateRendererTesty
 {
-    public class UnitTest1
+
+    public class TestModel
+    {
+
+        public List<string> Options { get; set; } = new() { "option1", "option2" };
+
+    }
+
+    public class RazorLightTemplateRendererTests
     {
 
         [Fact]
-        public async Task Test1Async()
+        public async Task RenderTemplate()
         {
             //arrange
             var engine = new RazorLightEngineBuilder()
-                // required to have a default RazorLightProject type,
-                // but not required to create a template from string.
-                .UseEmbeddedResourcesProject(typeof(UnitTest1))
-                 .SetOperatingAssembly(typeof(UnitTest1).Assembly)
+            // required to have a default RazorLightProject type,
+            // but not required to create a template from string.
+                .UseEmbeddedResourcesProject(typeof(RazorLightTemplateRendererTests))
+                 .SetOperatingAssembly(typeof(RazorLightTemplateRendererTests).Assembly)
                 .UseMemoryCachingProvider()
                 .Build();
 
             var model = new TestModel();
             var template = @"
-    @model RazorLightTemplateRendererTesty.TestModel;
+    @model Neuroglia.UnitTests.Cases.Data.TestModel;
+
     <select>
         @foreach(string option in this.Model.Options)
         {
@@ -33,12 +45,19 @@ namespace RazorLightTemplateRendererTesty
 ";
 
             //act
+            //var rendered = await this.TemplateRenderer.RenderTemplateAsync(template, model);
             using MemoryStream stream = new(Encoding.UTF8.GetBytes(template));
             string hash = Encoding.UTF8.GetString(await MD5.Create().ComputeHashAsync(stream));
             var rendered = await engine.CompileRenderStringAsync(hash, template, model);
 
             //assert
-            Assert.False(string.IsNullOrWhiteSpace(rendered));
+            rendered.Should().NotBeNullOrWhiteSpace();
+            rendered.Should().Contain("<select>");
+            rendered.Should().Contain($"<option value=\"{model.Options[0]}\">{model.Options[0]}</option>");
+            rendered.Should().Contain($"<option value=\"{model.Options[1]}\">{model.Options[1]}</option>");
+            rendered.Should().Contain("</select>");
         }
+
     }
+
 }
