0f65b13223e95bc8a8d57cc6e2b69562ce7576a4
Gregoire Poncelet <gregoire.poncelet@neuroglia.io>
2022-11-17 13:11:49 +0100

    Add project files.

---
+++ RazorLightTemplateRendererTesty/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RazorLightTemplateRendererTesty
+{
+
+    public class TestModel
+    {
+
+        public List<string> Options { get; set; } = new() { "option1", "option2" };
+
+    }
+
+}
---
+++ RazorLightTemplateRendererTesty/UnitTest1.cs
@@ -0,0 +1,44 @@
+using RazorLight;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace RazorLightTemplateRendererTesty
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public async Task Test1Async()//2.0.0-rc.3
+        {
+            //arrange
+            var engine = new RazorLightEngineBuilder()
+                // required to have a default RazorLightProject type,
+                // but not required to create a template from string.
+                .UseEmbeddedResourcesProject(typeof(UnitTest1))
+                 .SetOperatingAssembly(typeof(UnitTest1).Assembly)
+                .UseMemoryCachingProvider()
+                .Build();
+
+            var model = new TestModel();
+            var template = @"
+    @model RazorLightTemplateRendererTesty.TestModel;
+    <select>
+        @foreach(string option in this.Model.Options)
+        {
+            <option value=""@option"">@option</option>
+        }
+    </select>
+";
+
+            //act
+            using MemoryStream stream = new(Encoding.UTF8.GetBytes(template));
+            string hash = Encoding.UTF8.GetString(await MD5.Create().ComputeHashAsync(stream));
+            var rendered = await engine.CompileRenderStringAsync(hash, template, model);
+
+            //assert
+            Assert.False(string.IsNullOrWhiteSpace(rendered));
+        }
+    }
+}
